b067d442d569a1b1eec2b06ae1929d87bcb67473
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-08 00:11:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipe={
   eggs: "3",
   flour: "1 cup",
   milk: "200ml"
-}
+};
 function updateObjectWithKeyAndValue(recipe,salt,onetbspoon){
   var newrecipe=Object.assign({},recipe,{[salt]:'onetbspoon'});
   return newrecipe;
